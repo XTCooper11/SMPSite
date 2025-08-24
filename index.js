b77f4ef1c09d49bde5f8dfcf6e33983b3810a473
OnlyVSUser <onlyvisualstudiouser@outlook.com>
2025-08-24 01:21:40 -0500

    Whole website

---
+++ index.js
@@ -0,0 +1,35 @@
+function loadPropertyPage() {
+    window.location.href = 'Property.html';
+};
+
+function loadXTCPage() {
+    window.location.href = 'XTCooper4611.html';
+};
+
+function loadHomePage() {
+    window.location.href = 'index.html';
+}
+
+function loadGalaxyRulerPage() {
+    window.location.href = 'GalaxyRuler2620.html';
+};
+
+function loadSharkPage() {
+    window.location.href = 'Shark.html';
+}
+
+function loadGovernmentPage() {
+    window.location.href = 'Government Property.html';
+}
+
+function loadLawsPage() {
+    window.location.href = 'Laws.html';
+}
+
+function loadLaw1Page() {
+    window.location.href = 'Laws Pages/Law1.html';
+}
+
+function loadLaw2Page() {
+    window.location.href = 'Laws Pages/Law 2.html';
+}
